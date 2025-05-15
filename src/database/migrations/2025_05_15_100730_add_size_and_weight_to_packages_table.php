c0789d3d18fd3514d3c7513a2120274892fdffbe
Tnovyloo <loobrome@gmail.com>
2025-05-15 18:33:08 +0200

    weight and size of package implemented in db and views

---
+++ src/database/migrations/2025_05_15_100730_add_size_and_weight_to_packages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->enum('size', ['S', 'M', 'L'])->nullable()->after('collected_date');
+            $table->unsignedInteger('weight')->nullable()->after('size'); // weight in grams
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->dropColumn(['size', 'weight']);
+        });
+    }
+};
